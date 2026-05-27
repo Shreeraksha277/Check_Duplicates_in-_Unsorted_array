6b13a247a5e27f01eb4f1d8f732771113be353ab
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-05-27 14:07:13 +0530

    Update sort.java

--- sort.java
+++ sort.java
@@ -1,10 +1,10 @@
 class Solution  {
-         public boolean containsDuplicate(int[]  nums) {
-         Set<Integer> set = new HashSet<>();
-           for(int i=0;i<nums.length;i++) {
-                  if(set.contains(nums[i])) return true;
-            set.add(nums[i]);
-          }  
+          public boolean containsDuplicate(int[]  nums) {
+          Set<Integer> set = new HashSet<>();
+            for(int i=0;i<nums.length;i++) {
+                   if(set.contains(nums[i])) return true;
+             set.add(nums[i]);
+           }  
          return false;
     }
 }
