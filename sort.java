95cc0b3c9b930c216dc757a3c050ff0cb1a0820b
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-06-29 11:28:23 +0530

    Update sort.java

--- sort.java
+++ sort.java
@@ -2,7 +2,7 @@
             public boolean containsDuplicate(int[]  nums) {
           Set<Integer> set = new HashSet<>();
             for(int i=0;i<nums.length;i++) {
-                    if(set.contains(nums[i])) return true;
+                     if(set.contains(nums[i])) return true;
                set.add(nums[i]);
             }  
          return false;
